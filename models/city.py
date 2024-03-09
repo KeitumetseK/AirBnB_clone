e52dfa02262e3525ff62e3836f95bbff23f89f55
KeitumetseK <keitumetselovely@gmail.com>
2024-03-09 03:28:17 -0800

    AirBnB

---
+++ models/city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines the City class."""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Represents a city.
+
+    Attributes:
+        state_id (str): The ID of the state the city belongs to.
+        name (str): The name of the city.
+    """
+
+    state_id = ""
+    name = ""
